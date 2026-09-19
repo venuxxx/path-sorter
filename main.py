7046aca57e039ed6a92b190665a53687fc86446a
Timur Shapovalov <sapovalovtimurs@gmail.com>
2026-09-19 17:59:32 +0300

    created a func that move files to their directories

--- main.py
+++ main.py
@@ -1,8 +1,8 @@
 from pathlib import Path
 
 WORK_DIRECTORY = Path().home() / "Downloads"
-
-extensions = {
+MAIN_DIRECTORY = Path().home()
+EXTENSIONS = {
     ".mp4": "Music",
     ".mp3": "Music",
     ".doc": "Documents",
@@ -17,5 +17,21 @@ extensions = {
     ".svg": "Pictures",
 }
 
-for item in WORK_DIRECTORY.iterdir():
-    print(item.name)
+def move_file_to_its_directory(item: Path):
+    if not item.is_file():
+        return
+    suffix = item.suffix.lower()
+    if suffix not in EXTENSIONS:
+        return
+    target_directory: Path = MAIN_DIRECTORY / EXTENSIONS[suffix]
+    target_directory.mkdir(parents=True, exist_ok=True)
+    item.rename(target_directory / item.name)
+
+
+def main():
+    if WORK_DIRECTORY.exists():
+        for item in WORK_DIRECTORY.iterdir():
+          move_file_to_its_directory(item)
+
+if __name__ == "__main__":
+    main()
